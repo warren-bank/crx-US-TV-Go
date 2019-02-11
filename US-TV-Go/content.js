9556f704775c219903d4251a2b821dd7b2112625
Warren R Bank <warren.r.bank@gmail.com>
2019-02-10 22:41:18 -0800

    refactoring: minor

--- US-TV-Go/content.js
+++ US-TV-Go/content.js
@@ -33,6 +33,34 @@ var payload = function(){
   catch(err) {}
 }
 
+var get_hash_code = function(str){
+  var hash, i, char
+  hash = 0
+  if (str.length == 0) {
+    return hash
+  }
+  for (i = 0; i < str.length; i++) {
+    char = str.charCodeAt(i)
+    hash = ((hash<<5)-hash)+char
+    hash = hash & hash  // Convert to 32bit integer
+  }
+  return Math.abs(hash)
+}
+
+var inject_function = function(_function){
+  var inline, script, head
+
+  inline = _function.toString()
+  inline = '(' + inline + ')()' + '; //# sourceURL=crx_extension.' + get_hash_code(inline)
+  inline = document.createTextNode(inline)
+
+  script = document.createElement('script')
+  script.appendChild(inline)
+
+  head = document.head
+  head.appendChild(script)
+}
+
 var inject_options = function(){
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get(['open_in_webcast_reloaded'], (result) => {
@@ -41,20 +69,6 @@ var inject_options = function(){
       resolve()
     })
   })
-}
-
-var inject_function = function(_function){
-  var inline, script, head
-
-  inline = document.createTextNode(
-    '(' + _function.toString() + ')()'
-  )
-
-  script = document.createElement('script')
-  script.appendChild(inline)
-
-  head = document.head
-  head.appendChild(script)
 }
 
 var inject_options_then_function = function(_function){
