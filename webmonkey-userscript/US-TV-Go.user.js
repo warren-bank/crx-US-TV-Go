3146136d2a1e408430c77fc4faf8d029e78e9ed6
Warren R Bank <warren.r.bank@gmail.com>
2020-10-28 20:02:06 -0700

    v2.0.3: update script to compensate for changes to website

--- webmonkey-userscript/US-TV-Go.user.js
+++ webmonkey-userscript/US-TV-Go.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         US TV Go
 // @description  Watch videos in external player.
-// @version      2.0.2
+// @version      2.0.3
 // @match        https://ustvgo.tv/*
 // @match        https://tvguide.to/*
 // @icon         http://ustvgo.tv/favicon.ico
@@ -15,7 +15,20 @@
 // @copyright    Warren Bank
 // ==/UserScript==
 
-// =============================================================================
+// ============================================================================= common
+
+var get_referer_url = function() {
+  var referer_url
+  try {
+    referer_url = unsafeWindow.top.location.href
+  }
+  catch(e) {
+    referer_url = unsafeWindow.location.href
+  }
+  return referer_url
+}
+
+// ============================================================================= iframe
 
 var get_hls_url = function() {
   if (unsafeWindow.filePath)
@@ -99,22 +112,7 @@ var get_hls_url = function() {
   return hls_url
 }
 
-// =============================================================================
-
-var get_referer_url = function() {
-  var referer_url
-  try {
-    referer_url = unsafeWindow.top.location.href
-  }
-  catch(e) {
-    referer_url = unsafeWindow.location.href
-  }
-  return referer_url
-}
-
-// =============================================================================
-
-var process_page = function() {
+var process_iframe_window = function() {
   var hls_url = get_hls_url()
 
   if (hls_url) {
@@ -132,33 +130,54 @@ var process_page = function() {
 
     GM_startIntent.apply(this, args)
   }
+
+  return !!hls_url
 }
 
-// =============================================================================
+// ============================================================================= parent
+
+var get_iframe_url = function() {
+  var urls = [
+    'https://tvguide.to/clappr.php',
+    'https://tvguide.to/',
+    'https://ustvgo.tv/clappr.php'
+  ]
+
+  var iframe
+  for (var i=0; (i < urls.length) && !iframe; i++) {
+    iframe = document.querySelector('iframe[src^="' + urls[i] + '"]')
+  }
+
+  return (iframe) ? iframe.getAttribute('src') : null
+}
+
+var process_parent_window = function() {
+  var iframe_url = get_iframe_url()
+
+  if (iframe_url) {
+    var headers = ['Referer', get_referer_url()]
+
+    var args = [iframe_url]
+
+    for (var i=0; i < headers.length; i++) {
+      args.push(headers[i])
+    }
+
+    GM_loadUrl.apply(this, args)
+  }
+
+  return !!iframe_url
+}
+
+// ============================================================================= common bootstrap
 
 var init = function() {
-  if (unsafeWindow.location.hostname.toLowerCase() === 'ustvgo.tv') {
-    var iframe = document.querySelector('iframe[src^="https://tvguide.to/"]')
-    if (iframe) {
-      var url     = iframe.getAttribute('src')
-      var headers = ['Referer', get_referer_url()]
+  if (process_iframe_window())
+    return
 
-      var args = [
-        url
-      ]
-
-      for (var i=0; i < headers.length; i++) {
-        args.push(headers[i])
-      }
-
-      GM_loadUrl.apply(this, args)
-    }
-  }
-  else {
-    process_page()
-  }
+  process_parent_window()
 }
 
-// =============================================================================
-
 init()
+
+// =============================================================================
